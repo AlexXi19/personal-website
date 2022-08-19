7f0f2b076ab0eeeee139604cd8c7d7a5f8727ff8
AlexXi19 <alex2001314jjj@gmail.com>
2022-08-19 01:12:23 -0700

    feat: add style and stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,39 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
 
-<slot />
+<div class="flex flex-row">
+	<div class="w-60 h-screen shadow-md bg-white px-1">
+		<ul class="relative mt-20">
+			<li class="relative">
+				<a
+					class="flex items-center font-bold text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+					href="#!"
+					data-mdb-ripple="true"
+					data-mdb-ripple-color="dark"
+				>
+					Home
+				</a>
+			</li>
+			<li class="relative">
+				<a
+					class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+					href="#!"
+					data-mdb-ripple="true"
+					data-mdb-ripple-color="dark">Stuff 1</a
+				>
+			</li>
+			<li class="relative">
+				<a
+					class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+					href="#!"
+					data-mdb-ripple="true"
+					data-mdb-ripple-color="dark">Stuff 2</a
+				>
+			</li>
+		</ul>
+	</div>
+	<div class="mt-20">
+		<slot />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,30 @@
-<script lang='ts'>
-import { supabase } from "$lib/supabase";
-import type { definitions } from "$lib/types/supabase";
-
-async function getMessage(): Promise<string | undefined> {
-	const { data, error } = await supabase.from<definitions['messages']>("messages").select("*");
-	return data?.[0].text;
-}
-
-let message = getMessage();
-
-
+<script lang="ts">
+	import svelteLogo from '$lib/assets/svelte-logo.svg';
+	import supabaseLogo from '$lib/assets/supabase-logo-wordmark--light.png';
+	export let data;
+	let message = data.message;
 </script>
 
-<h1>Alex's personal Website</h1>
-{#await message then value}
-	{#if value}
-		<p>{value}</p>
+<div class="max-w-5xl p-10 font-mono text-lg ">
+	<h1 class="font-semibold">Alex's personal Website</h1>
+	{#if message}
+		<p>{message}</p>
 	{/if}
-{/await}
-
+	<h2 class="flex flex-row">
+		Connect with me on:
+		<a
+			class="text-blue-400 h-5 pl-3"
+			href="https://www.linkedin.com/in/alex-xi-9a6527214/"
+			target="”_blank”"
+		>
+			Linkedin</a
+		>
+		<a class="text-blue-400 h-5 pl-3" href="https://github.com/AlexXi19" target="”_blank”">GitHub</a
+		>
+	</h2>
+	<h2 class="flex flex-row items-center">
+		Built with:
+		<img src={svelteLogo} alt="Svelte" class="pb-0.5 h-5 pl-3" />
+		<img src={supabaseLogo} alt="Supabase" class="pb-0.5 h-5 pl-3" />
+	</h2>
+</div>
