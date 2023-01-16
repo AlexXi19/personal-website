daf6d31c0b4d20874a0c0b580782c952005dce23
AlexXi19 <alex2001314jjj@gmail.com>
2023-01-16 02:01:08 -0800

    fix: fix prerender

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,13 +1,19 @@
-export async function load({ params }: any) {
+import type { ServerLoad } from '@sveltejs/kit';
+
+export const load: ServerLoad = async ({ params, setHeaders }) => {
 	const post = await import(`../blogs/${params.slug}.md`);
 	const { title, date } = post.metadata;
 	const content = post.default;
+
+	// 2 days
+	setHeaders({
+		age: '172800',
+		'cache-control': 'public, max-age=172800'
+	});
 
 	return {
 		content,
 		title,
 		date
 	};
-}
-
-export const prerender = 'auto';
+};
