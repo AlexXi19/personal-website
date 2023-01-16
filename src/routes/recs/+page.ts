11651f77a5b49144be085ed06c5c397d4512fe1f
AlexXi19 <alex2001314jjj@gmail.com>
2023-01-16 00:46:23 -0800

    feat: cache reading list

--- src/routes/recs/+page.ts
+++ src/routes/recs/+page.ts
@@ -1,8 +1,14 @@
 import { getRecs } from '$lib/utils/recs';
 import type { Load } from '@sveltejs/kit';
 
-export const load: Load = async ({ url }) => {
+export const load: Load = async ({ url, setHeaders }) => {
 	const recs = await getRecs(0, 1000);
+
+	setHeaders({
+		age: '86400',
+		'cache-control': 'public, max-age=86400'
+	});
+
 	return {
 		recs
 	};
