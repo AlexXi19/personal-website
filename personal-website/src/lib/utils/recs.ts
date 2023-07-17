12ac9eae565094ddfbade96aea62e647decc1ba6
AlexXi19 <alex2001314jjj@gmail.com>
2023-07-17 02:20:31 -0700

    feat: more self hosting

--- personal-website/src/lib/utils/recs.ts
+++ personal-website/src/lib/utils/recs.ts
@@ -1,5 +1,6 @@
 import { supabase } from '$lib/supabase';
 import type { definitions } from '$lib/types/supabase';
+import { PUBLIC_READWISE_TOKEN } from '$env/static/public'
 
 interface ReadwiseBookResponse {
 	count: number;
@@ -51,7 +52,7 @@ export async function getReadwiseReads(opts: ReadwiseSearchParams): Promise<Read
 
 	const res = await fetch('https://readwise.io/api/v2/books?' + searchParams, {
 		headers: {
-			Authorization: 'Token ' + import.meta.env.VITE_READWISE_TOKEN
+			Authorization: 'Token ' + PUBLIC_READWISE_TOKEN
 		}
 	});
 	const data: ReadwiseBookResponse = await res.json();
