596a604fcc393591ecb1da62fcae0980f0f4721e
AlexXi19 <alex2001314jjj@gmail.com>
2023-08-20 01:01:59 -0700

    chore: more dashboards

---
+++ scripts/export-grafana.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+GRAFANA_URL="http://localhost:3000"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$SCRIPT_DIR/../resources/infra/dashboards"
+
+DASHBOARDS=$(curl -s -H "Authorization: Bearer ${API_KEY}" "${GRAFANA_URL}/api/search?folderIds=0" | jq -r '.[] .uid')
+
+for DASHBOARD in $DASHBOARDS; do
+  DASHBOARD_JSON=$(curl -s -H "Authorization: Bearer ${API_KEY}" "${GRAFANA_URL}/api/dashboards/uid/${DASHBOARD}")
+  DASHBOARD_TITLE=$(echo $DASHBOARD_JSON | jq -r '.dashboard.title')
+  
+  echo $DASHBOARD_JSON | jq '.dashboard' > "${DASHBOARD_TITLE// /_}.json"
+done
+
+echo "Dashboards exported!"
